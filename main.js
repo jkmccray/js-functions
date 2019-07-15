0ef0ff11c3e891cc021163da3c8566020d6fe8cd
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-15 10:11:34 -0500

    Fixed ChickenMonkey

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Practice: ChickenMonkey
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
   // Divide the current number by 5, and check if the remainder is 0
-  if (currentNumber % 5 === 0) {
-      console.log("Chicken") // Multiples of 5 will appear
+  if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+    console.log("ChickenMonkey")
+  } else if (currentNumber % 5 === 0) {
+      console.log("Chicken") // Multiples of 5 will say "Chicken"
   } else if (currentNumber % 7 === 0) {
-      console.log("Monkey")
+      console.log("Monkey") // Multiples of 7 will say "Monkey"
   } else {
       console.log(currentNumber)
   }
